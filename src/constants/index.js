346b03d1ea732d096044779dd4eee07b88d2f9d5
Sambhavi Shubhi <sambhavishubhi580@gmail.com>
2025-01-10 18:24:55 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,253 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Dog-API',
+    desc: 'The Dog API website is a fun and interactive platform where users can explore, search for, and learn about various dog breeds. It integrates with a public Dog API, allowing users to access real-time information and images of dogs. The site is built using modern frontend technologies and is designed to be visually appealing, user-friendly, and responsive.',
+    subdesc:
+      'The site fetches data from the Dog API using Axios. The data includes details about various dog breeds, their images, and other interesting facts. These API calls are made dynamically as the user interacts with different features, such as the search bar or breed explorer.',
+    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Interior Web design website',
+    desc: 'interior web design website project, I created a platform that showcases elegant and modern design solutions for various spaces. The website features a clean, intuitive interface with sections dedicated to services, a gallery of design concepts, and an interactive contact form. Users can easily navigate through the homepage, services, about, and contact sections, exploring a range of interior design options tailored to their needs.',
+    subdesc:
+      'temperature project, I developed a weather application that allows users to check real-time weather conditions by entering a location in the search bar. The app provides detailed information, including temperature, humidity, and weather descriptions, with a simple and user-friendly interface.',
+    href: 'https://gadget-galore.netlify.app/',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Traveler Website',
+    desc: 'A Traveler Website is an online platform that provides various services and information related to travel. It is designed to help users plan, book, and manage their trips more efficiently. A well-structured traveler website typically includes the following key features and sections:',
+    subdesc:
+      'The website also integrates with maps, provides mobile responsiveness, and supports multiple languages and currencies. Its goal is to provide a seamless, user-friendly experience for travelers, making it easier to explore new destinations and manage all aspects of their travel plans.',
+    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: '3-D Rotating Cube',
+    desc: 'A 3D Rotating Cube Project is an interactive web application that displays a 3D cube which continuously rotates around its axis. Using technologies like React and three.js, the project enables users to visualize and interact with the cube in a three-dimensional space. The cube can be customized with different textures, colors, and animations.',
+    subdesc:
+      'This project demonstrates the power of 3D graphics in web development, providing a dynamic and engaging user experience while showcasing skills in frontend development, 3D rendering, and JavaScript programming.',
+    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Sandal Shopping Website',
+    desc: 'A Sandal Shopping Website is an e-commerce platform that allows users to browse, select, and purchase various types of sandals. It includes features like product categories, filters (size, color, material), detailed product descriptions, user reviews, secure payment options, and order tracking.',
+    subdesc:
+      'The website aims to provide a smooth shopping experience with an intuitive interface for discovering and buying sandals online.',
+    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Bachelor of Technology',
+    pos: 'Electrical Engineering',
+    duration: '2020 - 2023',
+    title: "I am successfully completed my Bachelor of Technology in Electrical Engineering from Bankura Unnayani Institute of Engineering, affiliated with Maulana Abul Kalam Azad University of Technology (MAKAUT), in the year 2023. I have graduated with an impressive CGPA of 8.69, demonstrating strong academic performance and a solid foundation in electrical engineering concepts.",
+    icon: '/assets/framer.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Intenship',
+    pos: 'Web Developer',
+    duration: '2023 - 2024',
+    title: "I have concluded a web development internship at Intellioz Solutions India Pvt. Limited. During this internship, I have gained hands-on experience in web technologies, honed her skills in frontend development, and worked on real-world projects that enhanced her understanding of responsive design, UI/UX principles, and modern web frameworks. This experience has contributed to her professional growth in the field of web development.",
+    icon: '/assets/figma.svg',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Course & Notion',
+    pos: 'Java Developer',
+    duration: '2024 - Present',
+    title: " I have completed a comprehensive course for Java development at Besant Technology, Bangalore. During this course, I have did projects and Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+    icon: '/assets/notion.svg',
+    animation: 'salute',
+  },
+];
